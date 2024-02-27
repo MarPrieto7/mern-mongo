42fa16c8b976ee70402625aa007a0c1f3ace8ff0
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-02-27 15:27:47 +0100

    Change controller

--- Backend/controllers/todo.controller.js
+++ Backend/controllers/todo.controller.js
@@ -25,3 +25,14 @@ export const createTodo = async (req, res) => {
         res.status(500).json({ message: "No muestra datos del servidor" }, error);
     }
 };
+
+export const updateTodo = async (req, res) => {
+    const id = req.params.id
+    console.log(id);
+   try {
+    await Todo.updateOne({_id:id}, req.body);
+    res.status(200).json({message: "Todo ok", id});
+   } catch (error) {
+    res.status(500).json({ message: "No muestra datos del servidor", error});
+   }
+}
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getTodos, createTodo } from '../controllers/todo.controller.js'; // Importa createTodo también
+import { getTodos, createTodo, updateTodo } from '../controllers/todo.controller.js'; // Importa createTodo también
 
 const router = express.Router();
 router.get("/", getTodos);
 router.post("/", createTodo); // Usa createTodo aquí
+router.put("/update/:id", updateTodo);
 
 export default router;
